82f8e00fd307eca8d99c651239f4540c320a4520
FruitSamuraiG <147321490+FruitSamuraiG@users.noreply.github.com>
2023-10-08 21:32:59 +0300

    Функция заполнения массива случайными числами

--- arrInput.h
+++ arrInput.h
@@ -1,15 +1,15 @@
 #pragma once
-
+#include <ctime>
+#include <random>
 #include <iostream>
 #include <string>
 #include <fstream>
 
 using namespace std;
 
+void LoadArrayFromFile(size_t& sizeOfArray, int* & arrayOfNumbers);
+void MakeRandomArray(size_t& sizeOfArray, int* & arrayOfNumbers);
 void StringReverse(string& text);
-
 string StringInputCheck();
-
 bool IsAReservedName(string string1);
-
 void LoadArrayFromFile(size_t& sizeOfArray, int* arrayOfNumbers);
