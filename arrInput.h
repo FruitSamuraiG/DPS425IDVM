b460f7cddaf3f21bdadf604582c520c3bce265c6
LegendSuperCode <147321739+LegendSuperCode@users.noreply.github.com>
2023-10-08 21:16:14 +0300

    Заголовочный файл для ввода данных из файла

--- arrInput.h
+++ arrInput.h
@@ -1,4 +1,15 @@
 #pragma once
 
-void LoadArrayFromFile(size_t& sizeOfArray, int* & arrayOfNumbers);
-void MakeRandomArray(size_t& sizeOfArray, int* & arrayOfNumbers);
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+void StringReverse(string& text);
+
+string StringInputCheck();
+
+bool IsAReservedName(string string1);
+
+void LoadArrayFromFile(size_t& sizeOfArray, int* arrayOfNumbers);
