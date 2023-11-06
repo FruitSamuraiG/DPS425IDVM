098453871c44bfe5eee30a0006cf5c1df5700946
LegendSuperCode <147321739+LegendSuperCode@users.noreply.github.com>
2023-11-06 19:09:28 +0300

    обновленный файл сортировки с комментариями

--- gnomeSort.cpp
+++ gnomeSort.cpp
@@ -1,9 +1,9 @@
 #include "gnomeSort.h"
 using namespace std;
 
-void GnomeSort(int*& arrayOfNumber, size_t & sizeOfArray) {
-	for (size_t i = 1; i < sizeOfArray;) {
-		if (arrayOfNumber[i - 1] < arrayOfNumber[i]) {
+void GnomeSort(int*& arrayOfNumber, size_t & sizeOfArray) {    //функция гномьей сортировки
+	for (size_t i = 1; i < sizeOfArray;) {                     //ничего не возвращает
+		if (arrayOfNumber[i - 1] < arrayOfNumber[i]) {         //в качестве аргумента принимает массив по ссылке, а также размер массива по ссылке
 			swap(arrayOfNumber[i - 1], arrayOfNumber[i]);
 			if (i > 1) i--;
 		}
